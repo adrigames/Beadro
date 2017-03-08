7601d5ff13bc2962e0e1d20554dc572b6ed25cc0
Theigigna <ignacioriberadiaz@gmail.com>
2017-03-08 04:33:26 +0100

    Avanzando
    Lo que te he dicho por wasap

--- practica1.pl
+++ practica1.pl
@@ -1,15 +1,20 @@
 % Autor:
 % Fecha: 16/02/2017
 
-chico([albert,paul,tom,derek,richard,louis,michael,charles,sam,steve,will,anthony,billy,henry])
-chica([tiffany,natalie,roxanne,sarah,sabrina,cindy,emma])
-pelo_negro([albert,tom,derek,richard,louis,charles,steve,henry,tiffany,sarah,sabrina,cindy])
-pelo_rubio([paul,michael,sam,will,anthony,billy,natalie,roxanne,emma])
-ropa_roja([albert,paul,richard,louis,sam,steve,anthony,henry,natalie,sarah,cindy])
-ropa_verde([tom,derek,michael,charles,will,billy,tiffany,roxanne,sabrina,emma])
-feliz([albert,tom,richard,michael,charles,steve,will,anthony,tiffany,natalie,roxanne,sabrina,cindy,emma])
-triste([paul,derek,louis,sam,billy,henry,sarah])
-gafas([albert,michael,charles,anthony,natalie,sabrina])
-ojos_azules([albert,richard,louis,sam,will,billy,natalie,roxanne,sabrina])
-ojos_marrones([paul,tom,derek,michael,charles,steve,anthony,henry,tiffany,sarah,cindy,emma])
-personaje([albert,paul,tom,derek,richard,louis,michael,charles,sam,steve,will,anthony,billy,henry,tiffany,natalie,roxanne,sarah,sabrina,cindy,emma])
+hola:- writeln('funciona').
+
+chico([albert,paul,tom,derek,richard,louis,michael,charles,sam,steve,will,anthony,billy,henry]).
+chica([tiffany,natalie,roxanne,sarah,sabrina,cindy,emma]).
+pelo_negro([albert,tom,derek,richard,louis,charles,steve,henry,tiffany,sarah,sabrina,cindy]).
+pelo_rubio([paul,michael,sam,will,anthony,billy,natalie,roxanne,emma]).
+ropa_roja([albert,paul,richard,louis,sam,steve,anthony,henry,natalie,sarah,cindy]).
+ropa_verde([tom,derek,michael,charles,will,billy,tiffany,roxanne,sabrina,emma]).
+feliz([albert,tom,richard,michael,charles,steve,will,anthony,tiffany,natalie,roxanne,sabrina,cindy,emma]).
+triste([paul,derek,louis,sam,billy,henry,sarah]).
+gafas([albert,michael,charles,anthony,natalie,sabrina]).
+ojos_azules([albert,richard,louis,sam,will,billy,natalie,roxanne,sabrina]).
+ojos_marrones([paul,tom,derek,michael,charles,steve,anthony,henry,tiffany,sarah,cindy,emma]).
+
+personaje([albert,paul,tom,derek,richard,louis,michael,charles,sam,steve,will,anthony,billy,henry,tiffany,natalie,roxanne,sarah,sabrina,cindy,emma]).
+
+preguntas([chico,chica,pelo_negro,pelo_rubio,ropa_roja,ropa_verde,feliz,triste,gafas,ojos_azules,ojos_marrones]).
--- programa.pl
+++ programa.pl
@@ -1,16 +1,106 @@
 % Autor:
 % Fecha: 04/03/2017
 
-consult(practica1).
+%consult(practica1).
+
+%-----------------mostrar una lista
+print_l([]):- writeln('').
+print_l([X|L]):- write(X), write(', '), print_l(L).
+
+imprimirpreguntas:- preguntas(X),print_l(X).
+%-----------------empezar el juego
+inicio:-
+writeln('Quien es quien'),
+writeln('Elija dificultad'),
+writeln('\tFacil: 1.'),
+writeln('\tDificil: 2.'),
+writeln('\tNo quiero jugar: 3.'),
+read(Dificultad),
+jugar(Dificultad),
+writeln('Gracias por jugar').
+
+%------------------inicia el juego con dificultad 1 o 2 o sale del juego
+
+jugar(Dificultad):-
+Dificultad=:=1 ->
+writeln('Jugando al facil...'),
+inicializar(Chicos,Chicas,PN,PR,RR,RV,F,T,G,OA,OM,Personajes,Preguntas),
+print_l(PN)
+%hueco formula juego facil
+.
+
+jugar(Dificultad):-
+Dificultad=:=2 ->
+writeln('Jugando al dificil...'),
+writeln('Modo no disponible, compre el dlc')
+%hueco formula juego dificil
+.
+
+jugar(Dificultad):-
+Dificultad=:=3 ->
+writeln('Saliendo.');
+writeln('No hay mas opciones, elija una de las disponibles.'),inicio
+.
+
+
+%---------- sorteo jugador 1: empieza maquina / empieza jugador
+
+sorteo_inicial(X):- random(0,2,X),
+(X=:=0 -> writeln('Empieza el jugador.');
+writeln('Empieza la maquina.')).
+
+%-----------inicializar
+
+inicializar(Chicos,Chicas,PN,PR,RR,RV,F,T,G,OA,OM,Personajes,Preguntas):-
+chico(Chicos),
+chica(Chicas),
+pelo_negro(PN),
+pelo_rubio(PR),
+ropa_roja(RR),
+ropa_verde(RV),
+feliz(F),
+triste(T),
+gafas(G),
+ojos_azules(OA),
+ojos_marrones(OM),
+personaje(Personajes),
+preguntas(Preguntas).
+
+
+%-----------pertenencia elemento lista
+pertenece(C, [C|_]) :- !.
+pertenece(C, [_|R]) :- pertenece(C,R).
+
+%-----------eliminar elemento lista
+elimina(X,[X|X1],X1).
+elimina(X,[Y|Y1],[Y|Z1]):- elimina(X, Y1, Z1).
+
+%---------inicializar personajes
+
+inicializar_personajes(PersonajeJugador, PersonajeMaquina, ListaPersonajes):-
+
+personaje_jugador(PersonajeJugador,ListaPersonajes),
+personaje_maquina(PersonajeMaquina,PersonajeJugador,ListaPersonajes),
+writeln(PersonajeJugador),writeln(PersonajeMaquina).
+
+
+personaje_jugador(P,L):-
+length(L,N),random(0,N,Ns),
+nth0(Ns,L,P).
+
+personaje_maquina(Pj,P,L):-
+elimina(P,L,Ls),
+length(Ls,N),random(0,N,Ns)
+,nth0(Ns,Ls,Pj).
 
 %CONSULTAS AUXILIARES
 
 %posicion devuelve la posicion de un elemento.
-posicion(ELEMENTO,[ELEMENTO|_],0).
-posicion(_,[],_):-!,fail.
-posicion(ELEMENTO,[_|LIS],Posicion):-posicion(ELEMENTO,LIS,Posi), Posicion is Posi + 1.
+%posicion(ELEMENTO,[ELEMENTO|_],0).
+%posicion(_,[],_):-!,fail.
+%posicion(ELEMENTO,[_|LIS],Posicion):-posicion(ELEMENTO,LIS,Posi), Posicion is Posi + 1.
 
-imprimirLis([]).
-imprimirLis([ELEMENTO|LIS]):- writeln(ELEMENTO),imprimirLis(LISTA).
+%moneda(X):-random(0,2,X),write(X).
 
-moneda(X):-random(0,2,X),write(X).
+
+prueba_bool:- chico(X), pertenece(tiffany,X) -> writeln('funciona') ; writeln('no funciona').
