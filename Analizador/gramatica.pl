6a4e0661456180223559421efac3f138465a8365
Theigigna <ignacioriberadiaz@gmail.com>
2017-04-05 03:47:49 +0200

    Ya funcionan todas las frases
    Todas, las frases que pone en el pdf que deben funcionar ya funcionan.
    He puesto una funcion mas de testeo (parte4) en la que podemos poner mas
    frases, inventate algunas y ponlas si quieres.

--- Analizador/gramatica.pl
+++ Analizador/gramatica.pl
@@ -8,16 +8,32 @@
 oracion(o(OS)) --> oracion_simple(OS).
 oracion(o(OC)) --> oracion_compuesta(OC).
 
-%oraciones compuestas
-oracion_compuesta(ocm(OC)) --> oracion_coordinada(OC).
-oracion_compuesta(ocm(OR)) --> oracion_subordinada_relativo(OR).
-oracion_compuesta(ocm(OC,OR)) --> oracion_coordinada(OC), oracion_subordinada(OR).
-%oracion_compuesta(ocm(O, O2)) --> oracion(O), oracion(O2).
-oracion_coordinada(oc(O,C,O2)) --> oracion_simple(O), conjuncion(C), oracion_simple(O2).
-%subordinadas
-oracion_subordinada_relativo(or(GN,CS,GV)) --> grupo_nominal(GN), oracion_subordinada(CS), grupo_verbal(GV).
-oracion_subordinada_relativo(or(OS,CS,OS2)) --> oracion_simple(OS), oracion_subordinada.
+%oracion(osub(CS,OS)) --> conjuncion_subordinada_relativa(CS), oracion(OS).
+
 oracion_subordinada(osub(CS,OS)) --> conjuncion_subordinada(CS), oracion_simple(OS).
+%oracion_subordinada_relativo(or(GN,CS)) --> grupo_nominal(GN), oracion_subordinada(CS).
+oracion_compuesta(or(GN,CS,GV)) --> grupo_nominal_v2(GN), oracion_subordinada(CS), grupo_verbal(GV).
+oracion_compuesta(or(OS,CS)) --> oracion_simple(OS), oracion_subordinada(CS).%, oracion(OS2).
+oracion_compuesta(oc(GN,CS,GV,C,O)) --> grupo_nominal_v2(GN), oracion_subordinada(CS), grupo_verbal(GV), conjuncion(C), oracion_simple(O).
+oracion_compuesta(oc(GN,CS,GV,O)) --> grupo_nominal_v2(GN), oracion_subordinada(CS), grupo_verbal(GV), oracion_simple(O).
+
+%oraciones compuestas
+%oracion_coordinada(oc(O,C,O2)) --> oracion_simple(O), conjuncion(C), oracion(O2).
+
+oracion_compuesta(oc(O,C,O2)) --> oracion(O), conjuncion(C), oracion(O2).
+oracion_compuesta(oc(O,C,O2)) --> oracion_compuesta(O), conjuncion(C), oracion(O2).
+/*oracion_coordinada(oc(O,C,O2,C2,O3)) --> oracion_simple(O), conjuncion(C), oracion_simple(O2), conjuncion(C2), oracion_simple(O3).
+oracion_coordinada(oc(O,C,O2,C2,O3,C3,O4)) --> oracion_simple(O), conjuncion(C), oracion_simple(O2), conjuncion(C2), oracion_simple(O3), conjuncion(C3), oracion_simple(O4).
+*/
+  /*
+oracion_compuesta(ocm(OC)) --> oracion_coordinada(OC).
+oracion_compuesta(ocm(OC,OR)) --> oracion_coordinada(OC), oracion(OR).
+oracion_compuesta(ocm(OC,OR)) --> oracion_compuesta(OC), oracion_subordinada(OR).
+
+oracion_compuesta(ocm(OR)) --> oracion_subordinada_relativo(OR).*/
+%oracion_compuesta(ocm(O, O2)) --> oracion_subordinada_relativo(O), oracion(O2).
+%subordinadas
+
 %oracion simple
 oracion_simple(os(GN,GV)) --> grupo_nominal_v2(GN), grupo_verbal(GV).
 oracion_simple(os(GV)) --> grupo_verbal(GV).
@@ -30,46 +46,48 @@ grupo_nominal(gn(N)) --> nombre_comun(N).
 grupo_nominal(gn(NP)) --> nombre_propio(NP).
 grupo_nominal(gn(DET,N)) --> determinante(DET), nombre_comun(N).
 grupo_nominal(gn(DET,ADJ,N)) --> determinante(DET), adjetivo(ADJ), nombre_comun(N).
-grupo_nominal(gn(DET,N,ADJ)) --> determinante(DET), nombre_comun(N), adjetivo(ADJ).
+%grupo_nominal(gn(DET,N,GADJ)) --> determinante(DET), nombre_comun(N), grupo_adjetival(GADJ).%, adjetivo(ADJ).
 %complejos
-grupo_nominal_v2((GN)) --> grupo_nominal(GN).
+grupo_nominal_v2(gn(GN)) --> grupo_nominal(GN).
 grupo_nominal_v2(gn(GN, NP)) --> grupo_nominal(GN), nombre_propio(NP).
 grupo_nominal_v2(gn(GN, GADJ)) --> grupo_nominal(GN), grupo_adjetival(GADJ).
-grupo_nominal_v2(gn(GN, C, GN2)) --> grupo_nominal(GN), conjuncion(C), grupo_nominal_v2(GN2).
+grupo_nominal_v2(gn(GN, C, GN2)) --> grupo_nominal(GN), conjuncion(C), grupo_nominal(GN2).
+grupo_nominal_v2(gn(GN,GP)) --> grupo_nominal(GN), grupo_preposicional(GP).
 
 %grupos adjetivales
 grupo_adjetival(gadj(ADJ)) --> adjetivo(ADJ).
 grupo_adjetival(gadj(ADJ, GP)) --> adjetivo(ADJ), grupo_preposicional(GP).
-grupo_adjetival(gadj(ADV, GADJ)) --> adverbio(ADV), grupo_adjetival(GADJ2).
-%grupo_adjetival(gadj(GADJ, GADJ2)) --> grupo_adjetival(GADJ), grupo_adjetival(GADJ2).
-grupo_adjetival(gadj(ADJ, C, GADJ2)) --> adjetivo(ADJ), conjuncion(C), grupo_adjetival(GADJ2).
-grupo_adjetival(gadj(ADV, ADJ, C, GADJ2)) --> adverbio(ADV), adjetivo(ADJ), conjuncion(C), grupo_adjetival(GADJ2).
+grupo_adjetival(gadj(ADV, GADJ)) --> adverbio(ADV), grupo_adjetival(GADJ).
+grupo_adjetival(gadj(GADV, ADJ)) --> grupo_adverbial(GADV), adjetivo(ADJ).
+%grupo_adjetival(gadj(ADJ, C, GADJ2)) --> adjetivo(ADJ), conjuncion(C), grupo_adjetival(GADJ2).
+%grupo_adjetival(gadj(ADV, ADJ, C, GADJ2)) --> adverbio(ADV), adjetivo(ADJ), conjuncion(C), grupo_adjetival(GADJ2).
 
 
 
 %grupos adverbiales
-grupo_adverbial(gadv(ADV)) --> adverbio(ADV).
 grupo_adverbial(gadv(ADV, ADV2)) --> adverbio(ADV), adverbio(ADV2).
 grupo_adverbial(gadv(ADV, GN)) --> adverbio(ADV), grupo_nominal(GN).
 grupo_adverbial(gadv(ADV, GP)) --> adverbio(ADV), grupo_preposicional(GP).
+grupo_adverbial(gadv(ADV)) --> adverbio(ADV).
 
+%grupos verbales
+grupo_verbal(gv(V)) --> verbo(V).
+grupo_verbal(gv(V, GADJ)) --> verbo(V), grupo_adjetival(GADJ).
+grupo_verbal(gv(V, GADV)) --> verbo(V), grupo_adverbial(GADV).
+grupo_verbal(gv(V, GN)) --> verbo(V), grupo_nominal_v2(GN).
+grupo_verbal(gv(V, GN, GP)) --> verbo(V), grupo_nominal_v2(GN), grupo_preposicional(GP).
+grupo_verbal(gv(V, GP)) --> verbo(V), grupo_preposicional(GP).
+grupo_verbal(gv(V, GP, GP2)) --> verbo(V), grupo_preposicional(GP), grupo_preposicional(GP2).
+%grupo_verbal(gv(V, GADJ)) --> verbo(V), grupo_adjetival(GADJ).
+%grupo_verbal(gv(V, GN, GP, GP2)) --> verbo(V), grupo_nominal_v2(GN), grupo_preposicional(GP), grupo_preposicional(GP2).
 
 %grupos preposicionales
 grupo_preposicional(gp(P, GN)) --> preposicion(P), grupo_nominal_v2(GN).
 grupo_preposicional(gp(P, GADJ)) --> preposicion(P), grupo_adjetival(GADJ).
 grupo_preposicional(gp(P, GADV)) --> preposicion(P), grupo_adverbial(GADV).
 grupo_preposicional(gp(P, GV)) --> preposicion(P), grupo_verbal(GV).
-grupo_preposicional(gp(P, GP)) --> preposicion(P), grupo_preposicional(GP).
+%grupo_preposicional(gp(P, GP)) --> preposicion(P), grupo_preposicional(GP).
 
-%grupos verbales
-grupo_verbal(gv(V)) --> verbo(V).
-grupo_verbal(gv(V, GN, GP)) --> verbo(V), grupo_nominal_v2(GN), grupo_preposicional(GP).
-grupo_verbal(gv(V, GN, GP, GP2)) --> verbo(V), grupo_nominal_v2(GN), grupo_preposicional(GP), grupo_preposicional(GP2).
-grupo_verbal(gv(V, GN)) --> verbo(V), grupo_nominal_v2(GN).
-grupo_verbal(gv(V, GADJ)) --> verbo(V), grupo_adjetival(GADJ).
-grupo_verbal(gv(V, GADV)) --> verbo(V), grupo_adverbial(GADV).
-grupo_verbal(gv(V, GP)) --> verbo(V), grupo_preposicional(GP).
-grupo_verbal(gv(V, GP, GP2)) --> verbo(V), grupo_preposicional(GP), grupo_preposicional(GP2).
 
 %------Diccionario------
 
@@ -138,6 +156,7 @@ n(derecho).
 n(filosofia).
 n(cerveza).
 n(documentos).
+n(pelo).
 
 
 nombre_propio(np(X)) --> [X], {np(X)}.
@@ -150,6 +169,7 @@ np(irene).
 verbo(v(X)) --> [X], {v(X)}.
 v(ama).
 v(come).
+v(es).
 v(comen).
 v(toma).
 v(toman).
@@ -162,16 +182,21 @@ v(escala).
 v(sirve).
 v(lee).
 v(tiene).
-v(es).
 v(estudia).
 v(escribir).
+v(recoge).
 
 
 %adjetivos
 adjetivo(adj(X)) --> [X], {adj(X)}.
-adj(roja).
 adj(moreno).
+adj(morena).
+adj(roja).
+adj(rojas).
+adj(rubio).
 adj(alta).
+adj(alto).
+adj(fritas).
 adj(agil).
 adj(delicado).
 adj(potente).
@@ -181,8 +206,6 @@ adj(gris).
 adj(pequeño).
 
 %adverbios
-%adverbio(adv(X)) --> [X], {adv(X)}.
-%adv().
 adverbio(adv(X)) --> [X], {adv(X)}.
 adv(bastante).
 adv(muy).
--- Analizador/test.pl
+++ Analizador/test.pl
@@ -15,11 +15,13 @@ o5 :- oracion(X,[el,gato,grande,come,un,raton,gris],[]), draw(X).
 o6 :- oracion(X,[juan,estudia,en,la,universidad],[]), draw(X).
 o7 :- oracion(X,[juan,y,maria,comen,una,manzana,con,un,tenedor,y,un,cuchillo],[]), draw(X).
 
+parte2 :- p1,p2,p3,p4.
 p1 :- oracion(X,[juan,estudia,filosofia,pero,maria,estudia,derecho],[]),draw(X).
 p2 :- oracion(X,[juan,come,y,bebe,mientras,maria,lee],[]),draw(X).
 p3 :- oracion(X,[juan,que,es,muy,alto,tiene,el,pelo,rubio],[]),draw(X).
 p4 :- oracion(X,[juan,que,es,muy,alto,tiene,el,pelo,rubio,pero,maria,es,morena],[]),draw(X).
 
+parte3 :- a1,a2,a3,a4,a5,a6,a7,a8,a9,a10.
 a1 :- oracion(X,[juan,es,moreno,y,maria,es,alta],[]),draw(X).
 a2 :- oracion(X,[maria,toma,un,cafe,mientras,juan,recoge,la,mesa],[]),draw(X).
 a3 :- oracion(X,[juan,toma,cafe,y,lee,el,periodico],[]),draw(X).
@@ -31,5 +33,7 @@ a8 :- oracion(X,[juan,que,es,agil,escala,en,el,rocodromo,por,las,tardes],[]),dra
 a9 :- oracion(X,[juan,que,es,muy,delicado,come,solamente,manzanas,rojas],[]),draw(X).
 a10 :- oracion(X,[el,procesador,de,textos,que,es,una,herramienta,bastante,potente,sirve,para,escribir,documentos],[]),draw(X).
 
-
+%inventadas
+parte4 :- i1.
+i1 :- oracion(X,[juan,come,manzanas,por,las,tardes],[]),draw(X).
 
